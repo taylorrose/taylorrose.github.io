4155117d22813041622481f0467aa98f47163fc9
Taylor Rose <taylor.rose1@vivint.com>
2015-08-12 10:53:52 -0600

    Hi Ian

---
+++ _site/testing-results/5/embedd/RefactorMetrics.js
@@ -0,0 +1,17 @@
+var calculationsObj2 = [{	
+	total:function(){
+		console.log(this.calls+this.forms)
+	},
+	totalRR:function(){
+		
+		
+		return(this.visitors);
+		
+	},
+	visitors:2869,
+	calls: 100,
+	forms: 98,
+	name: "Default"
+
+
+}];
